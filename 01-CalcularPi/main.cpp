66a2aaeb86dd105a33506e2f7c0a089ae40292cb
Yanina Martinez <yamartinez@frba.utn.edu.ar>
2023-06-23 20:27:32 -0300

    main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream> 
 #include <iomanip>
+#include <math.h>
 
 using namespace std; 
 
 int main ()
 {
-    float pi=0, sg=0; 
-    for (int i = 0; i<250000; i++ ){
+    double pi=0, sg=0;
+    int i=0; 
+    double aux = 0;
+
+    do{
         sg= i%2==1 ? -1 : 1;
         pi += sg/ (2*i + 1);
-    }
-    pi *= 4;
-    cout<<"Valor de pi hasta 6 digitos decimales: "<<setprecision(6)<<fixed<<pi<<endl;
+        i++;
+        aux = trunc((4*pi - int(4*pi))*1000000)/1000000;
+    }while ( aux != 0.141592);
+    
+    cout<<"Valor de pi hasta 6 digitos decimales: "<<setprecision(6)<<fixed<<pi*4<<endl;
+    
     return 0;
 }
